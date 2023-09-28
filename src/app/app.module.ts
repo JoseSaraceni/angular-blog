0672a47acceb2431453e7a2e68e532263bbe284f
jose.saraceni <jose.vitor08@outlook.com>
2023-09-28 16:36:24 -0300

    componentes criados com estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { TitleComponent } from './componentes/title/title.component';
+import { BigCardComponent } from './componentes/big-card/big-card.component';
+import { LittleCardComponent } from './componentes/little-card/little-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    TitleComponent,
+    BigCardComponent,
+    LittleCardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/little-card/little-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-little-card',
+  templateUrl: './little-card.component.html',
+  styleUrls: ['./little-card.component.css']
+})
+export class LittleCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
